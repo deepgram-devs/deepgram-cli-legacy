810c0bcd9c5cb623496d6ea520351aac00563c3a
Luke Oliff <luke@lukeoliff.com>
2022-02-19 13:25:32 +0000

    add help header

---
+++ src/help.ts
@@ -0,0 +1,24 @@
+import { Help } from "@oclif/core";
+// import asciify from "asciify-image";
+import { logo } from "./logo";
+
+export default class CliHelp extends Help {
+  formatRoot() {
+    return [this.header(), this.version(), this.usage()].join("\n\n");
+  }
+
+  header() {
+    return ["", logo, "          The Deepgram API"].join("\n\n");
+  }
+
+  version() {
+    return this.section("VERSION", this.wrap(this.config.userAgent));
+  }
+
+  usage() {
+    return this.section(
+      this.opts.usageHeader || "USAGE",
+      this.wrap(`$ ${this.config.bin} [COMMAND]`)
+    );
+  }
+}
