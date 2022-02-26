9dcb5517bf4fff27410398dc25bf109e5fc1e890
Luke Oliff <luke@lukeoliff.com>
2022-02-26 21:38:28 +0000

    fix: simplify create command with new inquirer superpowers

--- src/commands/keys/create.ts
+++ src/commands/keys/create.ts
@@ -21,14 +21,45 @@ const validateApiKeyName = (input: string) => {
   throw Error("Please provide a name for the API key.");
 };
 
-export default class Keys extends AuthGuard {
+export default class CreateKey extends AuthGuard {
   static prompts = [
     {
       type: "input",
       name: "project",
       message: "Please enter a Project ID:",
-      require: true,
       validate: validateUuid,
+    },
+    {
+      type: "list",
+      name: "permission",
+      message:
+        "Select a permission level. More info here https://developers.deepgram.com/documentation/getting-started/roles/#account-roles:",
+      choices: [
+        {
+          short: "Owner",
+          value: "owner",
+          name: `Owner: Full access to create transcripts, manage team members and API keys,
+and change project and billing settings. Permission to monitor usage and logs.`,
+        },
+        {
+          short: "Admin",
+          value: "admin",
+          name: `Admin: Ability to create transcripts and manage team members and API keys.
+Permission to monitor project balances, usage, and logs.`,
+        },
+        {
+          short: "Member",
+          value: "member",
+          name: `Member: Permission to create transcripts and monitor usage and logs. Limited
+access to manage team members and API keys.`,
+        },
+      ],
+    },
+    {
+      type: "input",
+      name: "comment",
+      message: "Name your API key:",
+      validate: validateApiKeyName,
     },
   ];
 
@@ -42,69 +73,14 @@ export default class Keys extends AuthGuard {
 
   static description = "Create an API key for a project";
 
-  static examples = [
-    `$ deepgram keys create
-? Please enter a Project ID 24c4c8c2-bfb7-48fa-a1b5-709e7dq452d0
-...
-`,
-    `$ deepgram keys 7a0e1c0f-4b5a-5449-97d3-d36b7ec11c68
-...
-`,
-  ];
+  static examples = [];
 
   public async run(): Promise<void> {
-    let {
-      args: { project },
-    } = await this.parse(Keys);
-
-    if (typeof project !== "undefined") {
-      try {
-        validateUuid(project);
-      } catch (err: any) {
-        this.error(err);
-      }
-    } else {
-      project = (await inquirer.prompt(Keys.prompts)).project;
-    }
-
-    const answers = await inquirer.prompt([
-      {
-        type: "list",
-        name: "permission",
-        message:
-          "Select a permission level. More info here https://developers.deepgram.com/documentation/getting-started/roles/#account-roles:",
-        choices: [
-          {
-            short: "Owner",
-            value: "owner",
-            name: `Owner: Full access to create transcripts, manage team members and API keys,
-and change project and billing settings. Permission to monitor usage and logs.`,
-          },
-          {
-            short: "Admin",
-            value: "admin",
-            name: `Admin: Ability to create transcripts and manage team members and API keys.
-Permission to monitor project balances, usage, and logs.`,
-          },
-          {
-            short: "Member",
-            value: "member",
-            name: `Member: Permission to create transcripts and monitor usage and logs. Limited
-access to manage team members and API keys.`,
-          },
-        ],
-        required: true,
-      },
-      {
-        type: "input",
-        name: "comment",
-        message: "Name your API key:",
-        validate: validateApiKeyName,
-      },
-    ]);
+    let { args } = await this.parse(CreateKey);
+    args = await inquirer.prompt(CreateKey.prompts, args);
 
     const key = await this.deepgram.keys
-      .create(project, answers.comment, [answers.permission])
+      .create(args.project, args.comment, [args.permission])
       .catch((err) => this.error(err));
 
     this.log(tablize([key]));
