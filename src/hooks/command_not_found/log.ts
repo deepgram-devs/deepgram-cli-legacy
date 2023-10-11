8ea968c78bc9c6bd97c0e7703e5b47092b1728ed
Luke Oliff <luke@lukeoliff.com>
2023-10-11 15:30:49 +0100

    fix: remove hooks from build - they're completely unused (#154)

--- src/hooks/command_not_found/log.ts
+++
@@ -1,8 +0,0 @@
-import { Hook } from "@oclif/core";
-
-const hook: Hook<"command_not_found"> = async function (opts) {
-  process.stdout.write(`command "${opts.id}" not found\n`);
-  // this.exit();
-};
-
-export default hook;
